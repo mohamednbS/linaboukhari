07037569705d875a4f20295cc26d2576adb02a1d
mohamed nejib ben said <mohamed.nejib.bensaid@gmail.com>
2023-08-10 15:04:43 +0200

    jeudi

--- resources/views/menu/menutop.blade.php
+++ resources/views/menu/menutop.blade.php
@@ -9,6 +9,7 @@
         <div class="navbar-btn">
             <button type="button" class="btn-toggle-fullwidth"><i class="lnr lnr-arrow-left-circle"></i></button>
         </div>
+
     
         
         <div id="navbar-menu">
